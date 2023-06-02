34447efa9e94378a19e461d88e7de18deb2bd7be
Zhizheng Liu <candyboear@gmail.com>
2023-06-02 18:52:47 +0200

    remove unsed mapping thread

--- selfdrive/manager/process_config.py
+++ selfdrive/manager/process_config.py
@@ -37,7 +37,7 @@ procs = [
   NativeProcess("encoderd", "selfdrive/loggerd", ["./encoderd"]),
   NativeProcess("loggerd", "selfdrive/loggerd", ["./loggerd"], onroad=False, callback=logging),
   NativeProcess("modeld", "selfdrive/modeld", ["./modeld"]),
-  NativeProcess("mapsd", "selfdrive/navd", ["./map_renderer"]),
+  # NativeProcess("mapsd", "selfdrive/navd", ["./map_renderer"]),
   NativeProcess("navmodeld", "selfdrive/modeld", ["./navmodeld"]),
   NativeProcess("sensord", "selfdrive/sensord", ["./sensord"], enabled=not PC),
   NativeProcess("ui", "selfdrive/ui", ["./ui"], offroad=True, watchdog_max_dt=(5 if not PC else None)),
